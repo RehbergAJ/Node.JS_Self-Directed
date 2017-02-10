c7b10011ba6ea47fc134a46f0e7a46e85d190689
RehbergAJ <W0293942@nscc.ca>
2017-02-10 16:03:01 -0400

    GUI front end finished. Touched up Redis structure

--- app.js
+++ app.js
@@ -1,64 +1,65 @@
 
-//var redis = require('redis');
-//var client = redis.createClient();
+var redis = require('redis');
+var client = redis.createClient();
+// To rewrite to use redis
+var redisClient = redis.createClient();
 
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var messages = []; /* store message in array*/
+var nickname = client.nickname; //name of member
 
-io.on('connection', function(client) {
-    console.log('client connected...');
-
-    client.emit('message', { hello: 'world'});
-
-    client.on('messages', function(data){
-        var nickname = client.nickname;
-        console.log(data);
-        client.broadcast.emit("messages", data);
-        client.broadcast.emit("message", nickname + ": " + message);
-        client.emit("message", nickname + ": " + message);
-    });
-
-    client.on('join', function(name){    
-        client.nickname = name;
-        //notify other clients of chat joiner
-        client.broadcast.emit("add chatter", name);
-        /**redisClient.smembers('names', function(err, names){
-         * names.forEach(function(name){
-         * client.emit('add chatter', name);
-         * });
-        }); //emit all the currently logged in chatters to the newly connected client
-
-        //redisClient.sadd("chatters", names);
-        */
-    });
-});
-
-var storeMessage = function(name, data){
-    messages.push({name: name, data: data}); /* add message to end of array */
-    if (messages.length > 10){
-        messages.shift(); /*if more than 10 messages long, remove the first one */
-    }
-}
 
 io.sockets.on('connection', function(client) {
+        // changed join to use redis
+        //when the client joins
     client.on('join', function(name){
-        client.set('nickname', name);
-    messages.forEach(function(message){
-        client.emit("messages", message.name + ": " + message.data);
-    }); /** iterate through message array and emit message on the connecting client for each one */
-        client.broadcast.emit("chat", name + " joined the chat");
+        //add member to chat
+        client.broadcast.emit("add member", name);
+        //add member name to chat
+        redisClient.smembers('names', function(err, names){
+            names.forEach(function(name){
+                client.emit('add member', name);
+            });
+        });
+        redisClient.sadd("members", name);
     });
-    client.on("messages", function(message){
-        client.get("nickname", function(error, name){
-            client.broadcast.emit("messages", name + ": " + message);
-            client.emit("messages", name + ": " + message);
-            storeMessage(client.nickname, message);
-            storeMessage(name, message); /**when client sends a message call storeMessage */
+
+        //when the client messages
+        redisClient.lrange("messages", 0, -1, function(err, messages){
+            //emmit them in the right order
+            messages = messages.reverse() 
+            //reverse so they are emitted in the correct order
+            messages.forEach(function(message){
+                //parse into JSON object
+                message = JSON.parse(message); 
+                //emit to client
+                client.emit("messages", message.name + ": " + message.data);
+            });
+        });    
+
+    //when the client disconnects
+    client.on('disconnect', function(name){
+        client.get('nickname', function(err, name){
+            client.broadcast.emit("remove member", name);
+            redisClient.srem("members", name);
         });
     });
+
+    //--------------- using redis to store the messages
+    var storeMessage = function(name, data){
+        //turns into string the database can store it
+        var message = JSON.stringify({name: name, data: data});
+        //turns above into string to store into redis
+        redisClient.lpush("messages", message, function(err, response){
+            //keeps the first 10 strings, then leaves the rest
+            redisClient.ltrim("messages", 0, 9);
+        });
+    };  
+
+
 });
 
 app.get('/', function (require, response) {
@@ -67,17 +68,8 @@ app.get('/', function (require, response) {
 
 server.listen(8080);
 
-
-
-client.on('disconnect', function(name){
-    client.get('nickname', function(err, name){
-        client.broadcast.emit("remove chatter", name);
-
-        //redisClient.srem("chatters", namer);
-    });
-});
-
 //to database setting keys and values
+//using redis
 client.set("message1", "hello, yes this is dog");
 client.set("message2", "hello, no this is spider");
 //get from DB
@@ -85,28 +77,10 @@ client.get("message1", function(err,reply){
     console.log(reply);
 });
 
-/**
- * To rewrite to use redis
- * var redisClient = redis.createClient();
- * 
- * var storeMessage = function(name, data){
- *   var message = JSON.stringify({name: name, data: data});
- *  //turns above into string to store into redis
- *  redisClient.lpush("messages", message, function(err, response){
- *    redisClient.ltrim("messages", 0, 9);
- * });
- * }
- * 
- * change join to use redis
- * client.on('join', function(name){
- *  redisClient.lrange("messages", 0, -1, function(err, messages){
- *    messages = messages.reverse() 
- * //reverse so they are emitted in the correct order
- *    messages.forEach(function(message){
- *      //parse into JSON object
- *       message = JSON.parse(message); 
- *       client.emit("messages", message.name + ": " + message.data);
- *    });
- *   });
- * });
- */
+//add and remove members of the nameset
+client.sadd("names", "Doug");
+client.sadd("names", "Carl");
+client.sadd("names", "Glen");
+ 
+client.srem("names", "Carl");
+
