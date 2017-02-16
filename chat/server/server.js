68973bece0d7e658ddcc5aef9d876dc9b5eb428c
RehbergAJ <W0293942@nscc.ca>
2017-02-16 16:37:12 -0400

    Added Node routes, which will create, get and save messages as well as new users. Added Sockets which handles different chat rooms, as well as the default room, and switching between rooms. Added server.listen(2017) for this year for port. Test run half successful. Ran my console log, but didn't launch the HTML

--- chat/server/server.js
+++ chat/server/server.js
@@ -1,3 +1,4 @@
+/** ============================= DEPENDENCIES ============================== */
 //Import all the dependencies
 var express = require('express'),
     mongoose = require('mongoose'),
@@ -8,10 +9,10 @@ var express = require('express'),
     //telling express where to server the static files from
     app.use(express.static(__dirname + '/public'));
 
-    /**#ConnectionString setup the connection string */
-    mongoose.connect("mongodb://127.0.0.1:8888/chat");
+    /** #CONNECTION setup the connection string */
+    mongoose.connect("mongodb://127.0.0.1:2017/chat");
 
-    /**#Schema creating schema for chat */
+    /** #SCHEMA creating schema for chat */
     var ChatSchema = mongoose.Schema({
         created : Date,
         content: String,
@@ -19,10 +20,10 @@ var express = require('express'),
         room: String
     });
 
-    //create a modal fromt eh chat schema
+    //create a modal from the chat schema
     var chat = mongoose.model('Chat', ChatSchema);
 
-    //allow the use of CORS - CORS is an HTTP access control that allows access cross domain
+    // #CORS allow the use of CORS - CORS is an HTTP access control that allows access cross domain
     app.all('*', function(req, res, next){
         res.header("Access-Control-Allow-Origin", "*");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -35,14 +36,17 @@ var express = require('express'),
         }
     });
 
-    /**#Routes route for the index file  */
-    /**#Get */
+/** ============================= END DEPENDENCIES ============================== */    
+
+/** ============================== NODE ROUTES ==================================== */
+    /** #ROUTES route for the index file  */
+    /** #GET */
     app.get('/', function(req,res){
         //send index into the public directory
         res.sendfile('index.html');
     });
 
-    /**#Setup run on first launch to generate some chat history */
+    /** #SETUP run on first launch to generate some chat history */
     app.post('/setup', function(req,res){
         //Make the chat data array which will contain each object properties, and must match the schema props
         var pastChat = [{
@@ -80,4 +84,66 @@ var express = require('express'),
         res.send('created');
     });
     
-    
+
+    /** #GET make a list of chats filtered by room, get them then pass them to the JSON as an array */
+    app.get('/msg', function(req, res){
+    /** #FIND */
+        chat.find({
+            'room': req.query.room.toLowerCase()
+        }).exec(function(err, msgs){
+        /** #SEND */
+            res.json(msgs);
+        });
+    });
+/** ===================== END NODE ROUTES ==================================== */
+
+/** ============================ SOCKET ==================================== */
+
+/** #CONNECTION listen for the connection */
+    io.on('connection', function(socket){
+        //Global vars
+        var defaultRoom = 'general', 
+            rooms = ["General", "Node", "JavaScript", "CSS", "PHP"]
+
+        //Emit the rooms array
+        socket.emit('setup', {
+            rooms: rooms
+        });
+
+        //listen for new users
+        socket.on('new user', function(data){
+            data.room = defaultRoom;
+            //New user joins the defaultRoom
+            socket.join(defaultRoom);
+            //Emit the new user joining to the other users
+            io.in(defaultRoom).emit('user joined', data);
+        });
+
+        //Listen for room switch
+        socket.on('switch room', function(data){
+            //handles joins and leaves
+            socket.leave(data.oldRoom);
+            socket.join(data.newRoom);
+            io.in(data.oldRoom).emit('user left', data);
+            io.in(data.newRoom).emit('user joined', data);
+        });
+
+        /** #CHATLISTENER Listens for new chat messages */
+        socket.on('new message', function(data){
+            /** #CREATEMSG creating a new message */
+            var newMsg = new chat({
+                userName: data.username,
+                content: data.message,
+                room: data.room.toLowerCase(),
+                created: new Date()
+            });
+            /** #SAVE save it to the DB */
+            newMsg.save(function(err, msg){
+                //emit the message to the users in the room
+                io.in(msg.room).emit('message created', msg);
+            });
+        });
+    });
+/** ============================ END SOCKET ==================================== */
+server.listen(2017);
+console.log('Listening on port 2017 and beginning chat');
